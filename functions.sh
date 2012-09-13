899fc88b93f6cfa1dd050a0e736c8e99a2fc1c89
Jerome Forissier <jerome@taodyne.com>
2012-09-13 16:07:37 +0200

    Add posibility to: run_test.sh file.xl
    When runtest.sh receives a .xl file, it creates a temporary file
    with the contents of file.xl, wrapped between capture_begin/capture_end.

--- functions.sh
+++ functions.sh
@@ -3,3 +3,23 @@ die() {
   exit 1
 }
 
+_mktemp() {
+  case $(uname) in
+    MINGW*)
+      if [ "$1" = "-d" ] ; then
+        isdir = 1
+      fi
+      dst=$(echo $1 | sed 's/\.X\+/.'$$'/')
+      if [ "$isdir" ] ; then
+        mkdir -p $dst
+      else
+        touch $dst
+      fi
+      echo $dst
+      ;;
+    *)
+      mktemp $@
+      ;;
+  esac;
+}
+
